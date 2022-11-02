c7398033200da9965df6fde650e7c37f04757012
aymeric allain <aymericallain0@gmail.com>
2022-11-02 16:04:20 +0100

    DB-Per+Dia+Pat+Pres

---
+++ src/Entity/Patient.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PatientRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PatientRepository::class)]
+class Patient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Raison = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $DateHeureEntree = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $DateHeureSortie = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRaison(): ?string
+    {
+        return $this->Raison;
+    }
+
+    public function setRaison(string $Raison): self
+    {
+        $this->Raison = $Raison;
+
+        return $this;
+    }
+
+    public function getDateHeureEntree(): ?\DateTimeInterface
+    {
+        return $this->DateHeureEntree;
+    }
+
+    public function setDateHeureEntree(\DateTimeInterface $DateHeureEntree): self
+    {
+        $this->DateHeureEntree = $DateHeureEntree;
+
+        return $this;
+    }
+
+    public function getDateHeureSortie(): ?\DateTimeInterface
+    {
+        return $this->DateHeureSortie;
+    }
+
+    public function setDateHeureSortie(?\DateTimeInterface $DateHeureSortie): self
+    {
+        $this->DateHeureSortie = $DateHeureSortie;
+
+        return $this;
+    }
+}
--- src/Entity/Personne.php
+++ src/Entity/Personne.php
@@ -4,19 +4,33 @@ namespace App\Entity;
 
 use App\Repository\PersonneRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: PersonneRepository::class)]
-class Personne
+class Personne implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(nullable: true)]
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $Email = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column]
     private ?int $NumSecuriteSociale = null;
 
-    #[ORM\Column(nullable: true)]
+    #[ORM\Column]
     private ?int $NumAdeli = null;
 
     #[ORM\Column(length: 255)]
@@ -25,18 +39,77 @@ class Personne
     #[ORM\Column(length: 255)]
     private ?string $Prenom = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $Email = null;
-
     #[ORM\Column]
     private ?int $NumTel = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $MotDePasse = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->Email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getNumSecuriteSociale(): ?int
@@ -44,7 +117,7 @@ class Personne
         return $this->NumSecuriteSociale;
     }
 
-    public function setNumSecuriteSociale(?int $NumSecuriteSociale): self
+    public function setNumSecuriteSociale(int $NumSecuriteSociale): self
     {
         $this->NumSecuriteSociale = $NumSecuriteSociale;
 
@@ -56,7 +129,7 @@ class Personne
         return $this->NumAdeli;
     }
 
-    public function setNumAdeli(?int $NumAdeli): self
+    public function setNumAdeli(int $NumAdeli): self
     {
         $this->NumAdeli = $NumAdeli;
 
@@ -87,18 +160,6 @@ class Personne
         return $this;
     }
 
-    public function getEmail(): ?string
-    {
-        return $this->Email;
-    }
-
-    public function setEmail(string $Email): self
-    {
-        $this->Email = $Email;
-
-        return $this;
-    }
-
     public function getNumTel(): ?int
     {
         return $this->NumTel;
@@ -107,18 +168,6 @@ class Personne
     public function setNumTel(int $NumTel): self
     {
         $this->NumTel = $NumTel;
-
-        return $this;
-    }
-
-    public function getMotDePasse(): ?string
-    {
-        return $this->MotDePasse;
-    }
-
-    public function setMotDePasse(string $MotDePasse): self
-    {
-        $this->MotDePasse = $MotDePasse;
 
         return $this;
     }
