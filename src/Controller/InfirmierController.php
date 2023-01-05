0000b9eacbfa59305b4a2037ee3d63cc7b2cbd25
+GhostsFate81 <vincent.delestre@etu.univ-poitiers.fr>
2023-01-05 15:56:28 +0100

    code acceuil infirmier fini

--- src/Controller/InfirmierController.php
+++ src/Controller/InfirmierController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Personne;
+use App\Entity\Patient;
 use App\Service\CodeReminder;
 use App\Repository\LitRepository;
 use App\Repository\PatientRepository;
 use App\Repository\PersonneRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,53 +27,58 @@ class InfirmierController extends AbstractController
     }
 
     #[Route('/verification_code', name: 'verification_code')]
-    public function verification_code(CodeReminder $code_retenue, PatientRepository $patient, LitRepository $lit,PersonneRepository $roles ,ManagerRegistry $doctrine): Response
+    public function verification_code(PatientRepository $patients, LitRepository $lit,PersonneRepository $roles ,ManagerRegistry $doctrine, EntityManagerInterface $em): Response
     {
         $request = Request::createFromGlobals();
         $idMed = $request->request->get('medecin');
+        $code = $request->request->get('code');
+
         if($idMed != null || $idMed != "")
         {
             $assigner = true;
-            dump($idlit = $request->request->get('lit'));
-            dump($litpatient = $request->request->get('personne'));
+            $raison = $request->request->get('raison');
+            dump($idMed);
+            dump($raison);
+            dump($code);
         }
         else
         {
             $assigner = false;
-            $code = $request->request->get('code');
-            //$this->setParameter('code_retenue', $code);
-            $code_retenue->setCode($code);
             if($code == null || $code == "") // cas de code vide
             {
                 return $this->render('infirmier/verifcode.html.twig', ['salle' => null]);
             }
         }
-        //dump($this->getParameter('code_retenue'));
-        dump($code_retenue->getCode());
 
-        //$idpatient = $patient->findOneBy(['code_entre' => $this->getParameter('code_retenue')]);
-        $idpatient = $patient->findOneBy(['code_entre' => $this->getParameter('code_retenue')]);
 
-        if($idpatient == null) // case de code invalide
+        $Patient = $patients->findOneBy(['code_entre' => $code]);
+        
+        if($patients == null) // case de code invalide
         {
             return $this->render('infirmier/verifcode.html.twig', ['salle' => "introuvable"]);
         }
         
         $em = $doctrine->getManager();
-        $personne = $idpatient->getPersonne();
+        $personne = $Patient->getPersonne();
         $IdPersonne = $personne->getId();
         $salle = $lit->findSalle($IdPersonne);
         if($assigner == false)
         {
-            $litpatient = $lit->findOneBy(['salle'=> $salle])->getId();
             $medecin = $roles->findMedecin();
-            $Praison = $idpatient->getRaison();
-            return $this->render('infirmier/assignemedecin.html.twig',['lit'=> $litpatient, 'medecin' => $medecin, 'personne'=> $personne, 'salle' => $salle, 'raison'=>$Praison]);
+            return $this->render('infirmier/assignemedecin.html.twig',[ 'medecin' => $medecin, 'personne'=> $personne, 'salle' => $salle, 'code'=>$code]);
         }
         
+        $litpatient = $lit->findOneBy(['salle'=> $salle])->getId();
 
-        //$idpatient->setCodeEntre(null);
-        //$em->flush();
+        $Patient->setRaison($raison);
+        $medecin = $em->getRepository(Personne::class)->findOneBy(['id' => $idMed]);
+        dump($medecin);
+        $medecin->setPatient($Patient);
+        $Patient->setCodeEntre(null);
+        
+        $em->persist($medecin);
+        $em->persist($Patient);
+        $em->flush();
 
         return $this->render('infirmier/verifcode.html.twig', ['salle' => $salle, 'patient'=>$personne]);
     }
