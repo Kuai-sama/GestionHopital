8968cbec62587e5cf6ea5a70878b22d97ef048aa
THOMAS CHEVALIER <tcheva10@sfasp2mi.univ-poitiers.fr>
2023-02-03 11:29:38 +0100

    DateTime css

--- src/Form/AjoutPrescriptionType.php
+++ src/Form/AjoutPrescriptionType.php
@@ -7,6 +7,7 @@ use App\Entity\Prescription;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +15,23 @@ class AjoutPrescriptionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // Récupère la date d’aujourd’hui
+        $date = new \DateTime('now');
+        // On enlève les secondes
+        $date->setTime($date->format('H'), $date->format('i'), 0);
+
         $builder
             ->add('Unite')
-            ->add('DateFin')
-            ->add('date_debut')
+            ->add('DateFin',DateTimeType::class, [
+                'widget' => 'single_text',
+                'with_seconds' => false,
+                'data' => $date,
+            ])
+            ->add('date_debut',DateTimeType::class, [
+                'widget' => 'single_text',
+                'with_seconds' => false,
+                'data' => $date,
+            ])
             ->add('Status', ChoiceType::class,[
                 'choices' => [
                     'A faire' => 'A faire',
